e0f2aae8d7ddc99b4f87dbbd95c083b668b2f71f
SalmanM <goldgoldy276@gmail.com>
2012-04-20 18:03:12 +0430

    pics are optimize !

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function(){
 	var sld = document.getElementById("sld");
-	var aryImg=["images/1.png","images/2.png","images/3.png",
-				"images/4.png","images/5.png","images/6.png",
-				"images/7.jpg","images/8.jpg","images/9.jpg"
+	var aryImg=["images/0.jpg","images/1.jpg","images/2.jpg","images/3.jpg",
+				"images/4.jpg","images/5.jpg","images/6.jpg",
+				"images/7.jpg","images/8.jpg","images/9.jpg","images/10.jpg"
 				];
 	var index_img = 0;
 	function change_img(){
@@ -10,7 +10,7 @@ window.onload = function(){
 		index_img++;
 		index_img = (index_img >=aryImg.length)?0:index_img;	
 	}
-	var Intervl = setInterval(change_img,3000);
+	var Intervl = setInterval(change_img,1000);
 	sld.onclick = function (){
 		clearInterval(Intervl);	
 	}	
